200d7f97200bb3046ebafbb8b19fed3e14d4bc15
Ro-bu <robin.raid1@gmail.com>
2022-08-13 17:26:11 +0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+
+    return(
+        <div className="header">
+            <h1 className="header-title">COSMOS ODYSSEY</h1>
+            <button type="button" className="header-button">YOUR BOOKED FLIGHTS</button>
+        </div>
+    )
+}
